44c13d6a16943ceac68b4d0dd74b7f8b2e762f88
Thomas White <tdwhite1@sheffield.ac.uk>
2017-07-11 11:58:21 +0100

    JUnit 5 support

---
+++ leap/src/test/java/TestBarDrawer.java
@@ -0,0 +1,61 @@
+import com.sheffield.leapmotion.util.ProgressBar;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+/**
+ * Created by thomas on 11/07/17.
+ */
+public class TestBarDrawer {
+    String os = System.getProperty("os.name").toLowerCase();
+
+    public String osify(String s){
+        if (os.contains("windows")){
+            s = s.replace("∎", "#");
+        }
+        return s;
+    }
+
+    @Test
+    public void testHeaderedBarDrawer(){
+        ProgressBar.BarDrawer bd = new ProgressBar.HeaderedBarDrawer();
+
+        int bars = 21;
+
+        assertEquals("|0 ----- 50 ----- 100%|", bd
+                .getBarHeader(bars));
+
+        assertEquals(osify("|■■■■■■■■■■           | 50.0%"), bd.drawBar(bars,
+                0.5f));
+
+        assertEquals(bd.getBarHeader(bars).length(), bd.drawBar(bars, 0.5f).lastIndexOf("|")+1);
+    }
+
+
+    @Test
+    public void testHeaderedBarDrawerEmpty(){
+        ProgressBar.BarDrawer bd = new ProgressBar.HeaderedBarDrawer();
+
+        int bars = 21;
+
+        assertEquals("|0 ----- 50 ----- 100%|", bd
+                .getBarHeader(bars));
+
+        assertEquals(osify("|                     | 0.0%"), bd.drawBar(bars,
+                0.0f));
+    }
+
+    @Test
+    public void testHeaderedBarDrawerFull(){
+        ProgressBar.BarDrawer bd = new ProgressBar.HeaderedBarDrawer();
+
+        int bars = 21;
+
+        assertEquals("|0 ----- 50 ----- 100%|", bd
+                .getBarHeader(bars));
+
+        assertEquals(osify("|■■■■■■■■■■■■■■■■■■■■■| 100.0%"), bd.drawBar(bars,
+                1f));
+    }
+}
