dedca41543dc1a0231dd7f92f8ffd8a2fbaa4cea
Thomas D. White <tdwhite1@sheffield.ac.uk>
2017-07-18 15:52:30 +0100

    Temp files deleted after testing

--- leap/src/test/java/com/sheffield/leapmotion/runtypes/interaction/TestUserInteraction.java
+++ leap/src/test/java/com/sheffield/leapmotion/runtypes/interaction/TestUserInteraction.java
@@ -10,9 +10,16 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.Path;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
+
 
 /**
  * Created by thoma on 17/07/2017.
@@ -88,7 +95,24 @@ public class TestUserInteraction {
 
     @AfterAll
     public static void tearDown(){
-        tmpFile.delete();
+
+        try {
+            Files.walkFileTree(tmpFile.toPath(), new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    Files.delete(file);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                    Files.delete(dir);
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace(App.out);
+        }
     }
 
 }
